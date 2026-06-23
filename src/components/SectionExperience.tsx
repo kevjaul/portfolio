9245d3283fbf529db3074b18d4fe240bd33ee2ee
kevjaul <kevin31072002@gmail.com>
2026-06-23 14:28:12 +0200

    feat(experienceSection):  Switch linked to cards and timeline visualisation

--- src/components/SectionExperience.tsx
+++ src/components/SectionExperience.tsx
@@ -6,8 +6,6 @@ import Card from "./Card";
 
 type CardsInfos = {
   id: number;
-  x: number;
-  y: number;
   title: string;
   subtitle: string;
   description: string;
@@ -15,37 +13,40 @@ type CardsInfos = {
 function SectionExperience() {
   const { t } = useTranslation();
   const [activeCard, setActiveCard] = useState(0);
-  const experienceTypeSwitch = ["Formation", "Professionnel"];
-  const [selected, setSelected] = useState("Formation");
-  const formationCardsInfos: CardsInfos[] = useMemo(
-    () => [
-      {
-        id: 0,
-        x: 0.1,
-        y: 0.1,
-        title: "Ingénieur en Informatique",
-        subtitle: "Informatique et Systèmes d'Information",
-        description: "Temp",
-      },
-      {
-        id: 1,
-        x: 0.1,
-        y: 0.2,
-        title: "Master en Cybersécurité",
-        subtitle: "Sécurité des Systèmes d'Information",
-        description: "Temp",
-      },
-      {
-        id: 2,
-        x: 0.1,
-        y: 0.3,
-        title: "DUT Informatique",
-        subtitle: "Informatique",
-        description: "Temp",
-      },
-    ],
-    [],
-  );
+  const experienceTypeSwitch = ["Professionnel", "Formation"];
+  const [selected, setSelected] = useState("Professionnel");
+  const formationCardsInfos: CardsInfos[] = [
+    {
+      id: 0,
+      title: "Ingénieur en Informatique",
+      subtitle: "Informatique et Systèmes d'Information",
+      description: `Développement web utilisant le framework Symfony / PHP / Javascript
+        Architecture logicielle, micro-services et résilience des systèmes
+        Conception et gestion de bases de données relationnelles
+      `,
+    },
+    {
+      id: 1,
+      title: "Master en Cybersécurité",
+      subtitle: "Sécurité des Systèmes d'Information",
+      description: "Temp",
+    },
+    {
+      id: 2,
+      title: "DUT Informatique",
+      subtitle: "Informatique",
+      description: "Temp",
+    },
+  ];
+  const professionnelCardsInfos: CardsInfos[] = [
+    {
+      id: 0,
+      title: "Consultant IAM (Stage)",
+      subtitle: "IAM - Consulting - XML",
+      description: `Temp`,
+    },
+  ];
+  const [displayedCards, setDisplayedCards] = useState(professionnelCardsInfos);
   const containerRef = useRef<HTMLDivElement | null>(null);
   const [scrollProgress, setScrollProgress] = useState(0);
 
@@ -88,77 +89,79 @@ function SectionExperience() {
         <h1 className="text-6xl highlight font-bold text-center md:mb-0 mb-4">
           {t("sections.experience")}
         </h1>
-        <div className="w-full flex justify-center">
+        <div className="w-full flex justify-center mb-5">
           <div className="flex flex-col items-center gap-2 sm:w-[40%] w-[80%]">
             <div className="relative flex w-full">
               {experienceTypeSwitch.map((tab) => (
                 <button
                   key={tab}
-                  onClick={() => setSelected(tab)}
-                  className={`
-          relative py-2 text-lg font-medium transition-colors duration-300 w-1/2
-          ${
-            selected === tab
-              ? "text-white"
-              : "text-white/60 hover:text-[#a955f7]/80 hover:cursor-pointer"
-          }
-        `}
+                  onClick={() => {
+                    setSelected(tab);
+                    setDisplayedCards(
+                      tab === "Professionnel"
+                        ? professionnelCardsInfos
+                        : formationCardsInfos,
+                    );
+                  }}
+                  className={`relative py-2 text-lg font-medium transition-colors duration-300 w-1/2
+                    ${
+                      selected === tab
+                        ? "text-white"
+                        : "text-white/60 hover:text-[#a955f7]/80 hover:cursor-pointer"
+                    }
+                `}
                 >
                   {tab}
                 </button>
               ))}
-
               <div
-                className={`
-        absolute bottom-0 h-[2px]
-        bg-[#a955f7]
-        transition-all duration-300 ease-out
-      `}
+                className="absolute bottom-0 h-[2px] bg-[#a955f7] transition-all duration-300 ease-out"
                 style={{
-                  width: selected === "Formation" ? "30%" : "40%",
-                  left: selected === "Formation" ? "10%" : "55%",
+                  width: selected === "Professionnel" ? "40%" : "30%",
+                  left: selected === "Professionnel" ? "5%" : "60%",
                 }}
               />
             </div>
           </div>
         </div>
-        <div className="h-full" ref={containerRef}>
-          {formationCardsInfos.map((cardInfos, i) => {
+
+        <div ref={containerRef}>
+          {displayedCards.map((cardInfos, i) => {
             const state = i <= activeCard;
-
-            const x = cardInfos.x * window.innerWidth;
-            const y = cardInfos.y * window.innerHeight;
-
             return (
-              <motion.div
-                key={i}
-                className="relative w-[80%] max-w-[600px] pointer-events-auto h-fit"
-                style={{
-                  left: x,
-                  top: y,
-                }}
-                initial={{ opacity: 0, scale: 0.8, filter: "blur(10px)" }}
-                animate={{
-                  opacity: state ? 1 : 0.2,
-                  scale: state ? 1 : 0.85,
-                  filter: state ? "blur(0px)" : "blur(6px)",
-                  y: state ? -20 : 0,
-                }}
-                transition={{ duration: 0.6, ease: "easeOut" }}
-              >
-                <Card
-                  cardTitle={cardInfos.title}
-                  cardSubTitle={cardInfos.subtitle}
-                  cardFooter={
-                    <div className="mt-4 text-xs text-white/30">
-                      Experience {i + 1} / {formationCardsInfos.length}
-                    </div>
-                  }
-                  className="shadow-[0_0_40px_rgba(168,85,247,0.15)]"
+              <div className="relative w-full flex flex-row gap-4 mb-5 sm:ml-[10%] ml-[3%]">
+                <div className="flex flex-col items-center flex-shrink-0">
+                  <div className="w-4 h-4 rounded-full bg-white/15 mt-2" />
+
+                  <div className="flex-1 w-[3px] bg-white/15 mt-2 h-[95%]" />
+                </div>
+                <motion.div
+                  key={i}
+                  className="relative w-[80%] max-w-[600px] pointer-events-auto h-fit"
+                  style={{ left: 0, top: 0 }}
+                  initial={{ opacity: 0, scale: 0.8, filter: "blur(10px)" }}
+                  animate={{
+                    opacity: state ? 1 : 0.2,
+                    scale: state ? 1 : 0.85,
+                    filter: state ? "blur(0px)" : "blur(6px)",
+                    y: state ? -20 : 0,
+                  }}
+                  transition={{ duration: 0.6, ease: "easeOut" }}
                 >
-                  {cardInfos.description}
-                </Card>
-              </motion.div>
+                  <Card
+                    cardTitle={cardInfos.title}
+                    cardSubTitle={cardInfos.subtitle}
+                    cardFooter={
+                      <div className="mt-4 text-xs text-white/30">
+                        Experience {i + 1} / {formationCardsInfos.length}
+                      </div>
+                    }
+                    className="shadow-[0_0_40px_rgba(168,85,247,0.15)] mt-5"
+                  >
+                    {cardInfos.description}
+                  </Card>
+                </motion.div>
+              </div>
             );
           })}
         </div>
